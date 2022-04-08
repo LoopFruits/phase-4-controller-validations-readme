b12e2fa9331cd6f28fbf5c064fc0039015b00097
LoopFruits <Kensworldc@gmail.com>
2022-04-08 10:42:21 -0400

    completed assignment, with notes

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,7 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # we could add our rescue_from rather than using our manual validations with "rescue" below, we just need to add a private validation 
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,9 +11,17 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
+
+  # def create
+  #   # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+  #   bird = Bird.create!(bird_params)
+  #   render json: bird, status: :created
+  # end
 
   # GET /birds/:id
   def show
@@ -22,9 +32,18 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
-    render json: bird
+    bird.update!(bird_params)
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors}, status: :unprocessable_entity
   end
+
+
+  # def update
+  #   bird = find_bird
+  #   # update! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+  #   bird.update!(bird_params)
+  #   render json: bird
+  # end
 
   # DELETE /birds/:id
   def destroy
@@ -46,5 +65,9 @@ class BirdsController < ApplicationController
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
   end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
 
 end
